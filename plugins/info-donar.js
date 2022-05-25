0a3d564ea0163b77ad3d4842e8df56f17299993b
Tukiko300 <105240267+Tukiko300@users.noreply.github.com>
2022-05-25 17:42:55 -0400

    Update info-donar.js

--- plugins/info-donar.js
+++ plugins/info-donar.js
@@ -3,21 +3,17 @@
 let handler = async (m, { conn, usedPrefix, command }) => {
 let name = await conn.getName(m.sender)
 let donar =`
-*┏ ┅ ━━━━━━━━━ ┅ ━*
-*┇          「 𝐃𝐎𝐍𝐀𝐑 」*
-*┣ ┅ ━━━━━━━━━ ┅ ━*
-*┃ 𝙷𝙾𝙻𝙰 ${name} 💙*
-*┃*
-*[
-*┃ 👉🏻 𝙲𝙾𝙽𝚃𝙰𝙲𝚃𝙰𝙼𝙴 𝚂𝙸* 
-*┃ 𝙽𝙴𝙲𝙴𝚂𝙸𝚃𝙰𝚂 𝙼𝙰𝚂*
-*┃ 𝙳𝙰𝚃𝙾𝚂 𝚈 𝙿𝙰𝚁𝙰* 
-*┃ 𝙰𝙶𝚁𝙰𝙳𝙴𝙲𝙴𝚁𝚃𝙴 <𝟹*
-*┃ wa.me/595985168557*
-*┗ ┅ ━━━━━━━━━ ┅ ━*
+No hay nada que donar...
+
+Mi número primario/secundario se ha baneado
+¿Que hago? 
+
+Pues habla con la empresa, yo no tengo la culpa :v
+https://faq.whatsapp.com/android?lg=es&lc=PY&eea=0&anid=2c3ba50e-8911-471b-bb7d-472c5ef41545
+
 `.trim()
 conn.sendHydrated(m.chat, donar, wm, null, 'https://www.paypal.me/TheShadowBrokers133', '𝙿𝙰𝚈𝙿𝙰𝙻', null, null, [['𝙼𝙴𝙽𝚄 𝙿𝚁𝙸𝙽𝙲𝙸𝙿𝙰𝙻', '/menu']], m)}
 handler.help = ['donasi']
 handler.tags = ['info']
-handler.command = /^dona(te|si)|donar|apoyar$/i
+handler.command = /^dona(te|si)|whatsApp.com|apoyar$/i
 export default handler
